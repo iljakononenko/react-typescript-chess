7c4d7579d7556ad4b9169efd831a69bdd4df4825
iljakononenko <iljakononenko3@gmail.com>
2023-05-01 10:23:05 +0200

    [Frontend] - added timer and lost figures list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import React, {useEffect, useState} from 'react';
 import BoardComponent from "./components/BoardComponent";
 import {Board} from "./models/Board";
+import {Player} from "./models/Player";
+import {Colors} from "./models/Colors";
+import LostFigures from "./components/LostFigures";
+import Timer from "./components/Timer";
 
 function App() {
 
     const [board, setBoard] = useState(new Board())
+    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+    const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+    const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
+
+    function restart() {
+        const newBoard = new Board();
+        setBoard(newBoard)
+        setCurrentPlayer(whitePlayer)
+    }
+
+    useEffect(() => {
+        setCurrentPlayer(whitePlayer)
+    }, [])
+
+    function swapPlayer() {
+        setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+    }
 
   return (
     <div className="app">
-      <BoardComponent board={board} setBoard={setBoard} />
+        <Timer restart={restart} currentPlayer={currentPlayer} />
+        <BoardComponent board={board} setBoard={setBoard} currentPlayer={currentPlayer} swapPlayer={swapPlayer} />
+        <div>
+            <LostFigures
+                title={"Black figures"}
+                figures={board.lostBlackFigures}
+            />
+            <LostFigures
+                title={"White figures"}
+                figures={board.lostWhiteFigures}
+            />
+        </div>
     </div>
   );
 }
--- src/models/Board.ts
+++ src/models/Board.ts
@@ -6,9 +6,12 @@ import {King} from "./figures/King";
 import {Knight} from "./figures/Knight";
 import {Bishop} from "./figures/Bishop";
 import {Rook} from "./figures/Rook";
+import {Figure} from "./figures/Figure";
 
 export class Board {
     cells: Cell[][] = []
+    lostBlackFigures: Figure[] = [];
+    lostWhiteFigures: Figure[] = [];
 
     constructor() {
         this.initCells();
@@ -32,6 +35,8 @@ export class Board {
     public getCopyBoard(): Board {
         const newBoard = new Board();
         newBoard.cells = this.cells;
+        newBoard.lostWhiteFigures = this.lostWhiteFigures;
+        newBoard.lostBlackFigures = this.lostBlackFigures;
         return newBoard;
     }
 
